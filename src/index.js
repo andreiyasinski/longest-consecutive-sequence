d7ea1feb42c0436014abd016b301b1a1a6428a72
andreiyasinski <andrewtopol856@gmail.com>
2018-10-05 17:17:33 +0300

    :bulb:

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ if (arr.length == 0) return 0;
 
 for (let i = 0; i < arr.length; i++) {
 
+  if (arr[i] == arr[i+1]) {
+    continue;
+  }
   if (arr[i] + 1 == arr[i+1]) {
     sequencelength++;
   } else {
@@ -19,9 +22,7 @@ for (let i = 0; i < arr.length; i++) {
 
 temp.sort(compareNumeric);
 
-
 //console.log(temp);
-//console.log(temp[temp.length - 1])
 return temp[temp.length - 1];
 
 }
